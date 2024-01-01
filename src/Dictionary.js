5bcfbadabf6c381ff8f321fb54f5f22ab20dffcb
amandalynn1204 <amandalynn1204@icloud.com>
2024-01-01 15:19:40 -0500

    added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+
+    const apiKey = "9a7ca83bt1f54ebc3o8f9d804f5e2b0e";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
