b4bc566ab982ca2da98eba15773b92d13a0cf796
amandalynn1204 <amandalynn1204@icloud.com>
2024-01-02 11:22:19 -0500

    fixed a bug with synonyms component

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 export default function Synonyms(props) {
-  return (
-    <ul className="Synonyms">
-      {props.synonyms.map(function (synonym, index) {
-        return <li key={index}>{synonym}</li>;
-      })}
-    </ul>
-  );
+  if (props.synonyms === null) {
+    return null;
+  } else {
+    return (
+      <ul className="Synonyms">
+        {props.synonyms.map(function (synonym, index) {
+          return <li key={index}>{synonym}</li>;
+        })}
+      </ul>
+    );
+  }
 }
