ace35dc8f974a664258f86f256dbd3fb362ca228
amandalynn1204 <amandalynn1204@icloud.com>
2024-01-02 10:55:13 -0500

    added example of word to meaning component

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,10 +4,11 @@ export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      <h3>
-        <em>{props.meaning.partOfSpeech}</em>
-      </h3>
+      <h3>{props.meaning.partOfSpeech}</h3>
       <p>{props.meaning.definition}</p>
+      <p>
+        <em>{props.meaning.example}</em>
+      </p>
     </div>
   );
 }
